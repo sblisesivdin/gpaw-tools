6e42f241f1fa96070d424053392e22f0b745b6d3
Dr. Sefer Bora Lisesivdin <sblisesivdin@gmail.com>
2025-02-03 16:49:31 +0300

    Versions are corrected again.

--- gpawsolve.py
+++ gpawsolve.py
@@ -1804,7 +1804,7 @@ if __name__ == "__main__":
     # ------------------ End of Localisation Tables --------------------------
     
     # Version
-    __version__ = "v25.2.0"
+    __version__ = "v25.2.1"
 
     parser = ArgumentParser(prog ='gpawtools.py', description=Description, formatter_class=RawFormatter)
     parser.add_argument("-i", "--input", dest = "inputfile", help="Use input file for calculation variables (also you can insert geometry)")
